7b59bec848043eb03299a04a4cfbdc6242ff4713
Maihrendson <mcauaacademico@gmail.com>
2025-09-05 17:40:08 -0300

    commit de adição de animais com atributos

--- dia2/animais.py
+++ dia2/animais.py
@@ -2,16 +2,35 @@
 
 class Animal:
 
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
     def falar(self):
-        return "Emitir som"
+        return 0
     
+    def apresentar(self):
+        return f"Olá eu sou {self.nome}, e tenho {self.idade}"
 class Cachorro(Animal):
+
+    def __init__(self, nome, idade):
+        super().__init__(nome, idade)
     def falar(self):
         return "Au Au!"
-    
+
 class Gato(Animal):
+
+    def __init__(self, nome, idade):
+        super().__init__(nome, idade)
     def falar(self):
         return "Miau!"
     
-print(Cachorro().falar())
-print(Gato().falar())
+gato = Gato("Felino", 19)
+cachorro = Cachorro("Totó", 11)
+
+animal = [gato, cachorro]
+
+for Animal in animal:
+    print(Animal.apresentar())
+    print(Animal.falar())
+    print()
