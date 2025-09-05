941b64e3f55a5055dde426e2c497e1bdfbb0b003
Maihrendson <mcauaacademico@gmail.com>
2025-09-05 16:07:21 -0300

    Nesse commit adicionei uma função chamada de arredondamento para arredondar números para: Maior, Menor, Mais Próximo.

--- dia2/main.py
+++ dia2/main.py
@@ -1,8 +1,19 @@
 import math
 
+# Raiz Quadrada Simples
+
 def raiz_quadrada(num):
     raiz_quadrada = math.sqrt(num)
 
     return raiz_quadrada
 
 print(raiz_quadrada(4))
+
+# Arredondamento Inteligente
+def arrendondamento(value):
+
+    arredondar = math.floor(value), math.ceil(value), round(value)
+
+    return arredondar
+
+print("Valores arredondados", arrendondamento(70.54))
